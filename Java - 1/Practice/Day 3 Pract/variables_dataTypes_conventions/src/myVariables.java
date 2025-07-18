57628e46fdf12e14b56e825907556cb793c22397
Sagar Dhadke <peaceunique2003@gmail.com>
2025-07-18 17:41:49 +0530

    Day 3 Completed

---
+++ Java - 1/Practice/Day 3 Pract/variables_dataTypes_conventions/src/myVariables.java
@@ -0,0 +1,45 @@
+public class myVariables {
+    public static void main(String[] args){
+
+        //Variables
+
+        String name = "Hello World!";
+        System.out.println(name);
+
+        int num;
+        System.out.println(num = 123);
+
+        // num is variable and 123 is the value
+
+        // DataTypes
+
+        int age = 21;
+        String myName = "Sagar";
+        char letter = 'a';
+        boolean isMarried = false;
+        double price = 25.30;
+        float lowestPrice = 21.20f;
+        short lowerValue = 012345;
+        long longestValue = 8589568995685858527L;
+
+        //Naming Conventions
+
+        //camelCase
+
+        String firstName = "sagar";
+        String lastName = "Dhadke";
+
+        // snake_case
+        int my_bank_account_num = 1234567890;
+
+        //Jave Identifier
+
+        // A-z
+        // a-z
+        // 0-9
+        // _ and $
+
+
+
+    }
+}
