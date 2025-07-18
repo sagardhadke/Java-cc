66bde97bbfb8acf210d559942332c9a9c0b6ee36
Sagar Dhadke <peaceunique2003@gmail.com>
2025-07-18 17:31:36 +0530

    Day 3 Done

---
+++ Java - 1/Lectures/Day - 3 17-06-2025/variables_data_pract/src/allVariables.java
@@ -0,0 +1,35 @@
+public class allVariables {
+
+    public  static void main(String[] args){
+        String dev = "Java Dev";
+        System.out.println(dev);
+
+        int age = 21;
+        System.out.println(age);
+
+        boolean isMarried;
+        System.out.println(isMarried = false);
+
+        // int a = 5
+        // a is a variable and 5 is a value
+
+        // All DataTypes
+
+        int num = 124;
+        float pi = 3.27f;
+        double price = 35623.3232;
+        char favChar = 'S';
+        boolean isActive = true;
+        short lowPrice = 012456;
+        long highPrice = 8523084651238745333L;
+        String name = "Sagar Dhadke";
+
+
+
+
+    }
+
+
+
+
+}
---
+++ Java - 1/Lectures/Day - 3 17-06-2025/variables_data_pract/src/naming_Conventions.java
@@ -0,0 +1,20 @@
+public class naming_Conventions {
+
+    public  static void main(String[] args){
+
+        // Camel Case
+        String firstName = "Sagar";
+
+        // snake_case
+        String last_name = "Dhadke";
+
+        /*
+         The only allow (A-Z, [a-z], [0-9])
+            and special symbol => $  and _
+         */
+
+        // java identifiers are case-sensitive
+
+    }
+
+}
