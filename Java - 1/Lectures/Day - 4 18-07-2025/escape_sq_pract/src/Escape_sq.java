6f2c391911ce5720808717220323fa8393a9d8da
Sagar Dhadke <peaceunique2003@gmail.com>
2025-07-18 19:14:45 +0530

    Day 4

---
+++ Java - 1/Lectures/Day - 4 18-07-2025/escape_sq_pract/src/Escape_sq.java
@@ -0,0 +1,20 @@
+public class Escape_sq {
+    public static void main(String[] args){
+        // Escape Sequences in Java
+
+        // \t use for Tab
+        // \n use for next Line
+        // \b use for backspace
+        // \' use for single quote
+        // \" use for double quote
+        // \\ use for backslash
+
+        System.out.println("Hey this is \tSagar");
+        System.out.println("Hey this is \nSagar");
+        System.out.println("Hey this\b is Sagar");
+        System.out.println("Hey this is \'Sagar\' ");
+        System.out.println("Hey this is \"Sagar\" ");
+        System.out.println("Hey this is \\Sagar\\");
+
+    }
+}
---
+++ Java - 1/Practice/Challenge/Tasks 4/singal_print_patterns/src/LeftHalfPyramid.java
@@ -0,0 +1,5 @@
+public class LeftHalfPyramid {
+    public static void main(String[] args) {
+        System.out.println("        *\n      * *\n    * * *\n  * * * *\n* * * * *");
+    }
+}
