bfebdd845c789c55bec1e1f6064e50ad2d04810d
Sagar Dhadke <peaceunique2003@gmail.com>
2025-07-22 16:13:51 +0530

    Arithmetic Operators

---
+++ Java - 1/Lectures/Day - 5 21-07-2025/all_oprators/src/Arithmetic_Operators.java
@@ -0,0 +1,13 @@
+public class Arithmetic_Operators {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 2;
+
+        System.out.println("Addition = " + (a+b));
+        System.out.println("Subtraction = " + (a-b));
+        System.out.println("Multiplication = " + (a*b));
+        System.out.println("Division = " + (a/b));
+        System.out.println("Modulus = " + (5%2));
+
+    }
+}
--- Java - 1/Lectures/Day - 5 21-07-2025/all_oprators/src/Assignment.java
+++ Java - 1/Lectures/Day - 5 21-07-2025/all_oprators/src/Assignment.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Assignment {
+    public static void main(String[] args){
+
+
+        // Assigns the right-hand operand's value to the left-hand operand.
+        //Example: int a = 5;
+
+        int a = 5;
+
+    }
 }
